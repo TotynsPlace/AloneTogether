3db88f8c62111715cc8ef5e653df7cd6dd000fb0
Evan Sklarski <esklarski@gmail.com>
2020-04-18 02:41:32 -0700

    initial upload of online format

--- js/alonetogether.js
+++ js/alonetogether.js
@@ -1,13 +1,63 @@
-ORIENTATION = layout();
+PAGES = document.getElementsByClassName("page-container");
+ORIENTATION = "";
+CURRENT_INDEX = 0;
 
+window.addEventListener('resize', function(){ layout(); });
 
-function layout() {
-    if (window.innerWidth > window.innerHeight) { ORIENTATION = "landscape"; }
-    else { ORIENTATION = "portrait"; }
-
-    console.log(ORIENTATION);
+function init() {
+    layout();
 }
 
-window.addEventListener('resize', function(event){
-        layout();
-  });
+function layout() {
+    if (window.innerWidth > window.innerHeight) {
+        ORIENTATION = "landscape";
+    } else { ORIENTATION = "portrait"; }
+
+    if (ORIENTATION == "portrait" && PAGES[14].style.display == "none") {
+        transitionPages("error");
+        document.getElementById("story-footer").style.display = "none";
+    } else if (ORIENTATION == "landscape" && PAGES[14].style.display == "block") {
+        transitionPages();
+        document.getElementById("story-footer").style.display = "flex";
+    }
+}
+
+function navigate(direction) {
+    switch (direction) {
+        case "forward":
+            CURRENT_INDEX += 2;
+            if (CURRENT_INDEX > 12) { CURRENT_INDEX = 12 }
+            break;
+        case "back":
+            CURRENT_INDEX -= 2;
+            if (CURRENT_INDEX < 0) { CURRENT_INDEX = 0 }
+            break;
+    }
+
+    transitionPages();
+}
+
+function transitionPages(error) {
+    if (error == null) {
+        for (var i = 0; i < PAGES.length; i++) {
+            if (i == CURRENT_INDEX || i == CURRENT_INDEX + 1) {
+                if (PAGES[i].style.display == "none") {
+                    PAGES[i].style.display = "block";
+                }
+            } else {
+                PAGES[i].style.display = "none";
+            }
+        }
+    } else {
+        for (var i = 0; i < PAGES.length; i++) {
+            if (i == 14) {
+                if (PAGES[i].style.display == "none") {
+                    PAGES[i].style.display = "block";
+                }
+            } else {
+                PAGES[i].style.display = "none";
+            }
+        }
+    }
+}
+
