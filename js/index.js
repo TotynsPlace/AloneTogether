e1b4b9c63da8374c53cd4adccccbaf86fcaf5eaa
Evan Sklarski <esklarski@gmail.com>
2020-04-21 00:59:13 -0700

    link to read online from index - going live

--- js/index.js
+++ js/index.js
@@ -2,3 +2,8 @@ function download(format) {
     var target = "book/AloneTogether." + format;
     window.open(target);
 }
+
+function read() {
+    var target = "html/alonetogether.html";
+    location.href = target;
+}
